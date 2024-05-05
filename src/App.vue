ae5e118b9a21f18a9337afc24590c022d5175eb5
LucaElisio <personal.elix@gmail.com>
2024-05-05 15:27:10 +0200

    feat: comic books card dinamicaly with props

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import AppFooter from "./components/AppFooter.vue";
 import AppListIcon from "./components/AppListIcon.vue";
 
 
+
 export default {
   components: {
     AppHeader,
---
+++ src/components/ComicBooksCards.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    props: {
+                cardThumb: String,
+                cardSeries: String,
+            },
+
+    data() {
+        return {
+
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="cardThumb" :alt="CardSeries" />
+        <h5>{{cardSeries}}</h5>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    
+    img {
+        width: 100%;
+        aspect-ratio: 1;
+        object-fit: cover;
+        object-position: top;
+        margin-bottom: 1rem;
+    }
+
+    h5 {
+        text-transform: uppercase;
+    }
+
+
+</style>
